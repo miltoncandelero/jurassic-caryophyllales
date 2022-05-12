e5167b9d953440b3dd6f082ea328bb51a2bb28d4
Milton <miltoncandelero@gmail.com>
2022-05-12 16:37:49 -0300

    added assets for TP

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,47 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Ticker } from 'pixi.js'
+import { assets } from './assets';
+import { GameScene } from './scenes/GameScene';
+
+export const WIDTH = 1920;
+export const HEIGHT = 1080;
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: WIDTH,
+	height: HEIGHT,
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+window.addEventListener("resize", ()=>{
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);
 
-clampy.anchor.set(0.5);
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
 
-app.stage.addChild(clampy);
+	app.view.style.width = gameWidth + "px";
+	app.view.style.height = gameHeight + "px";
+
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginHorizontal + "px";
+
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
+});
+window.dispatchEvent(new Event("resize"));
+
+Loader.shared.add(assets);
+
+Loader.shared.onComplete.add(()=>{
+	const gamescene = new GameScene();
+	app.stage.addChild(gamescene);
+	Ticker.shared.add(()=>gamescene.update(Ticker.shared.deltaMS / 1000));
+});
+
+Loader.shared.load();
---
+++ src/scenes/GameScene.ts
@@ -0,0 +1,13 @@
+import { Container } from "pixi.js";
+
+export class GameScene extends Container {
+	constructor() {
+		super();
+		
+	}
+
+	public update (dt:number)
+	{
+		dt;
+	}
+}
